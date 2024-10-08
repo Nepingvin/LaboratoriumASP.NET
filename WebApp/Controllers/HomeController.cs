85681db38d6d7b42c504c8567a36f14c1c5adf35
anastasiya.kuzniatso <anastasiya.kuzniatso@microsoft.wsei.edu.pl>
2024-10-08 18:10:57 +0200

    Calculator.cshtml

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -23,9 +23,41 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult About(string op)
+    {
+        ViewBag.Op = op;
+        return View();
+    }
+
+    public IActionResult Calculator(string op, double x, double y)
+    {
+        switch (op)
+        {
+            case "add":
+                ViewBag.Result = x + y;
+                break;
+            case "sub":
+                ViewBag.Result = x - y;
+                break;
+            case "mul":
+                ViewBag.Result = x * y;
+                break;
+            case "div":
+                ViewBag.Result = x / y;
+                break;
+        }
+
+        return View();
     }
 }
